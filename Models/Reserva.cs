96cee70c427184dd920d394169c1c9d6db64ddac
gtretow <guilherme.tretow@gmail.com>
2024-08-20 17:29:42 -0300

    first commit

---
+++ Models/Reserva.cs
@@ -0,0 +1,70 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
+            if (Suite.Capacidade >= hospedes.Count)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
+                throw new Exception("Numero de hospedes maior que a capacidade da suite");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
+            if (Hospedes != null)
+            {
+                return Hospedes.Count;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            // TODO: Retorna o valor da diária
+            decimal valor = 0;
+
+            if (Hospedes != null)
+            {
+                valor = DiasReservados * Suite.ValorDiaria;
+            }
+
+            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
+            if (DiasReservados >= 10)
+            {
+                decimal novoValor = valor - (valor * 0.1M);
+                Console.WriteLine($"Houve um desconto de 10% para o valor da diaria: {valor} novo valor: {novoValor}");
+                valor = novoValor;
+                return valor;
+            }
+
+            return valor;
+        }
+    }
+}
